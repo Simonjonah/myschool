d0d1488883bd86ef3592bb4d9665a1b328cb967e
Simonjonah <simonudo74@gmail.com>
2023-10-08 10:05:01 -0700

    come

--- resources/views/dashboard/admin/eventedit.blade.php
+++ resources/views/dashboard/admin/eventedit.blade.php
@@ -61,14 +61,14 @@
                       <div class="form-group">
                         <label> Image</label>
                         <img style="width: 50%; height: 50%;" class="profile-user-img img-fluid"
-                            src="{{ URL::asset("/public/../$edit_events->images")}}"
+                            src="{{ URL::asset("/public/../$edit_events->logo")}}"
                             alt="User profile picture">
-                        <input type="file" @error('images')
-                        @enderror value="{{ old('images') }}" class="form-control" name="images"> 
+                        <input type="file" @error('logo')
+                        @enderror value="{{ old('logo') }}" class="form-control" name="logo"> 
                       </div>
                   
                     </div>
-                    @error('images')
+                    @error('logo')
                     <span class="text-danger">{{ $message }}</span>
                     @enderror 
                      
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -13,18 +13,7 @@
         </div>
     </section>
     <!--End Page Title-->
-<!-- Team Section Two -->
-	<section class="team-section-two" >
-		<div class="auto-container">
-			<!-- Sec Title -->
-			<div class="sec-title-three centered">
-				<div class="title">ABOUT MYSCHOOL.AFRICA</div>
-				<div class="text">MySCHooL.Africa is a philosophy that is conceptualised by Socialite Entrepreneurs to Advertise Schools across the globe through innovative digital platform thereby, helping them to improve in the market share.</div>
-			</div>
 
-		</div>
-	</section>
-	<!-- End Team Section Two -->
 	<!-- Services Section Three-->
 	<section class="services-section-three">
 		<div class="auto-container">
@@ -69,212 +58,63 @@
 			</div>
 		</div>
 	</section>
+<!-- About Section Four -->
+<section class="about-section-four">
+	<div class="auto-container">
 
-	<section class="team-section-two" >
-		<div class="auto-container">
-			<!-- Sec Title -->
-			<div class="sec-title-three centered">
-				<div class="title">Our Team</div>
-				<h2>We have an  <br> experience team Members</h2>
+		<div class="sec-title centered">
+			<div class="title" style="color: red;">About us</div>
+			{{-- <h2 style="color: purple;">For us at BRIXTONN, passion is our sole <br> driving force towards molding our pupils into their excellent states</h2> --}}
+		</div>
+
+		<div class="row clearfix">
+
+			<!-- Content Column -->
+			<div class="image-column col-lg-5 col-md-12 col-sm-12">
+				<div class="inner-column">
+					<div class="image-1"><img src="{{ asset('images/resource/bsch2.jpg') }}" alt=""></div>
+					<div class="image-2"><img src="{{ asset('images/resource/bsch2.jpg') }}" alt=""></div>
+				</div>
 			</div>
 
-			<div class="row clearfix">
+			<!-- Skills Column -->
+			<div class="skills-column col-lg-7 col-md-12 col-sm-12">
+				<div class="inner-column">
 
-<!--Team Block-->
-@foreach ($member_teams as $member_team )
-@if ($member_team->status == 'approved')
-<div class="team-block-two col-lg-4 col-md-6 col-sm-12">
-	<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
-		<div class="image">
-			<a href="team"><img style="height: 300px; width: 100%;" src="{{ URL::asset("/public/../$member_team->images")}}" alt="" title=""></a>
-			<ul class="social-box">
-				<li><a href="{{ $member_team->facebook }}" class="fa fa-facebook"></a></li>
-				<li><a href="{{ $member_team->twitter }}" class="fa fa-twitter"></a></li>
-				<li><a href="{{ $member_team->email }}" class="fa fa-whatsapp"></a></li>
-				<li><a href="{{ $member_team->likedin }}" class="fa fa-linkedin"></a></li>
-			</ul>
-		</div>
-		<div class="lower-content">
-			<h5><a href="team">{{ $member_team->lname }}, {{ $member_team->fname }}</a></h5>
-			<div class="designation">{{ $member_team->designation }}</div>
+					<!--Skills-->
+					<div class="skills style-two">
+
+						
+
+					<div class="text">
+						<p>Schools Up-date, is an information/examination management website. It enanle schools to send informations/Adverts directly to their pupls/students parent and word. It is also a system that help schools set-up their school examination records and results.</p>
+					</div>
+					<div class="text">
+						<p>To meat-up with the new technology age in Education, Schools Update is a most for every School. Students/Pupils can now check their results/school information straight from the comfort of their home.</p>
+					</div>
+					<div class="text">
+						<p>Schools Up-date comes with little and highly affordable cost. Registration is N5,000 for a period of six months, and 10,000 for a period of one year renewal system.</p>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
-</div>
-@else
+</section>
+<!-- End About Section Four -->
 	
-@endif
-	
-@endforeach 
-			</div>
-			<div class="text-center">
-				<a href="team" class="theme-btn btn-style-fourteen">View All Teams</a>
-			</div>
-		</div>
-	</section>
-	<!-- End Team Section Two -->
 <!-- Video Section -->
 	<section class="video-section" style="background-image:url(images/logo.jpg); margin-top: 40px;">
 		<div class="auto-container">
 			<img src="">
 			<div class="title">Video</div>
 			<h2>See how we work with <br>experience and   <span>creativity</span></h2>
-			<a href="https://www.youtube.com/watch?v=Fvae8nxzVz4" class="play-now" data-fancybox="gallery" data-caption=""><i class="icon flaticon-play-button" aria-hidden="true"></i><span class="ripple"></span></a>
+			<a href="#" class="play-now" data-fancybox="gallery" data-caption=""><i class="icon flaticon-play-button" aria-hidden="true"></i><span class="ripple"></span></a>
 		</div>
 	</section>
 	<!-- End Video Section -->
 
-	<!-- Services Section Nine -->
-	<section class="services-section-nine">
-		<div class="auto-container">
-			
-			<!-- Sec Title Two -->
-			<div class="sec-title-two centered">
-				<div class="title">Services</div>
-				
-			</div>
-			
-			<div class="clearfix">
-				
-				<!-- Services Block Twelve -->
-				<div class="services-block-twelve col-lg-6 col-md-12 col-sm-12">
-					<div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<img style="width: 100%; height: 300px;" src="images/background/bsch2.jpg" alt=""/>
-							<!-- Overlay Box -->
-							<div class="overlay-box">
-								<div class="overlay-inner">
-									<div class="content">
-										<h4>E-learning</h4>
-										<div class="text">Bringing quality and innovative learning to your door step through blended approaches.</div>
-										<div class="arrow-box fa fa-angle-right"></div>
-									</div>
-								</div>
-							</div>
-							<!-- Overlay Box Two -->
-							<div class="overlay-box-two">
-								<div class="overlay-inner-two">
-									<div class="large-icon flaticon-statistics"></div>
-									<div class="content-two">
-										<div class="icon-box">
-											<span class="icon flaticon-statistics"></span>
-										</div>
-										<h4><a href="#">E-learning</a></h4>
-
-										<h4><a href="#" class='btn btn-sucess'>View</a></h4>
-									</div>
-										
-								</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				
-				<!-- Services Block Twelve -->
-				<div class="services-block-twelve col-lg-6 col-md-12 col-sm-12">
-					<div class="inner-box wow fadeInRight" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<img style="width: 100%; height: 300px;" src="images/resource/my6.jpg" alt=""/>
-							<!-- Overlay Box -->
-							<div class="overlay-box">
-								<div class="overlay-inner">
-									<div class="content">
-										<h4>Career Counselling</h4>
-										<div class="text"> Gain professional advice through our experts on career and others life endeavours.</div>
-										<div class="arrow-box fa fa-angle-right"></div>
-									</div>
-								</div>
-							</div>
-							<!-- Overlay Box Two -->
-							<div class="overlay-box-two">
-								<div class="overlay-inner-two">
-									<div class="large-icon flaticon-graph"></div>
-									<div class="content-two">
-										<div class="icon-box">
-											<span class="icon flaticon-graph"></span>
-										</div>
-										<h4><a href="careers" class='btn btn-sucess'>Career Counselling</a></h4>
-										<h4><a href="careers" class='btn btn-sucess'>View</a></h4>
-										
-									</div>
-								</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				
-				<!-- Services Block Twelve -->
-				<div class="services-block-twelve col-lg-6 col-md-12 col-sm-12">
-					<div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<img style="width: 100%; height: 300px;" src="images/background/juv5.jpg" alt=""/>
-							<!-- Overlay Box -->
-							<div class="overlay-box">
-								<div class="overlay-inner">
-									<div class="content">
-										<h4>Contact Teacher's Services</h4>
-										<div class="text">Needed the best hands to guide your wards through challenging subjects with effective results.</div>
-										<div class="arrow-box fa fa-angle-right"></div>
-									</div>
-								</div>
-							</div>
-							<!-- Overlay Box Two -->
-							<div class="overlay-box-two">
-								<div class="overlay-inner-two">
-									<div class="large-icon flaticon-money-bag"></div>
-									<div class="content-two">
-										<div class="icon-box">
-											<span class="icon flaticon-money-bag"></span>
-										</div>
-										<h4><a href="view_teachers" class='btn btn-sucess'>Contact Teacher's Service</a></h4>
-										<h4><a href="view_teachers" class='btn btn-sucess'>View</a></h4>
-										
-									</div>
-								</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				
-				<!-- Services Block Twelve -->
-				<div class="services-block-twelve col-lg-6 col-md-12 col-sm-12">
-					<div class="inner-box wow fadeInRight" data-wow-delay="0ms" data-wow-duration="1500ms">
-						<div class="image">
-							<img style="width: 100%; height: 300px;" src="images/background/juv8.jpg" alt=""/>
-							<!-- Overlay Box -->
-							<div class="overlay-box">
-								<div class="overlay-inner">
-									<div class="content">
-										<h4>Advertise Schools</h4>
-										<div class="text">This platform 'ADVERTISE SCHOOLS' through quality digital marketing strategies and exposes their activities and programmes to their clientele across communities.</div>
-										<div class="arrow-box fa fa-angle-right"></div>
-									</div>
-								</div>
-							</div>
-							<!-- Overlay Box Two -->
-							<div class="overlay-box-two">
-								<div class="overlay-inner-two">
-									<div class="large-icon flaticon-exam"></div>
-									<div class="content-two">
-										<div class="icon-box">
-											<span class="icon flaticon-exam"></span>
-										</div>
-										<h4><a href="schoolregistration" class='btn btn-sucess'>Advertise Schools</a></h4>
-										<h4><a href="schoolregistration" class='btn btn-sucess'>View</a></h4>
-										
-									</div>
-								</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-	</section>
+	
 
 
 	<!-- About Section Four -->
